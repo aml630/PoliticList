eed9be1cc2a527523f11b5bcef7621f3a6ed7c2b
alex <alex.m.larson@gmail.com>
2016-07-19 15:00:07 -0700

    Add project files.

---
+++ PoliticList/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PoliticList.MvcApplication" Language="C#" %>
---
+++ PoliticList/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PoliticList.Startup))]
+namespace PoliticList
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
